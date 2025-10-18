5bed64bd3551be95ac7a99a78f5cf47392747933
Карагачев Андрей <86959168+Andrei-user@users.noreply.github.com>
2025-10-18 11:38:42 +0300

    fixa1ed22

--- Core/Src/pish_rcc_drv.c
+++ Core/Src/pish_rcc_drv.c
@@ -11,15 +11,17 @@
 #define PPL_SRC_HSE (1U)
 #define OFF_SET (6U)
 void PISH_RCC_Int(){
-	RCC->CR.B.HSION = ON;
+	RCC->CR.B.HSEON = ON;
 
 	uint32_t counter = 0;
-	while(RCC->CR.B.HSIRDY == OFF){
+	while(RCC->CR.B.HSERDY == OFF){
 		counter++;
 	}
 
+	RCC->PLLCFGR.R &= (0x3F); // SET M DIVIDDER
 	RCC->PLLCFGR.R |= (4<<0); // SET M DIVIDDER
-	RCC->PLLCFGR.R |= (100<<6); // SET N MULTIOPLAY
+	RCC->PLLCFGR.R &= (0x1FF<<OFF_SET); // SET N MULTIOPLAY
+	RCC->PLLCFGR.R |= (100<<OFF_SET); // SET N MULTIOPLAY
 	RCC->PLLCFGR.R &= ~(3<<16); // SET P DRIVER
 	RCC->PLLCFGR.B.PLLSRC = PPL_SRC_HSE; // SET hse
 
@@ -34,7 +36,7 @@ void PISH_RCC_Int(){
 	RCC->CFGR.R |= 2;
 
 
-	SysTick->LOAD.B.RELOAD = 16000 - 1;
+	SysTick->LOAD.B.RELOAD = 100000 - 1;
 	SysTick->CTRL.B.CLKSOURCE = ON;
 	SysTick->CTRL.B.TICKINT = ON;
 	SysTick->CTRL.B.ENABLE = ON;
