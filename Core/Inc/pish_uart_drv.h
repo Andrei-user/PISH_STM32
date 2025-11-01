8f30be07066d420f03b0397a3b856f3226933a3b
Карагачев Андрей <86959168+Andrei-user@users.noreply.github.com>
2025-11-01 10:47:13 +0300

    Добавление UART header

--- Core/Inc/pish_uart_drv.h
+++ Core/Inc/pish_uart_drv.h
@@ -4,10 +4,26 @@
  *  Created on: Nov 1, 2025
  *      Author: andre
  */
+#include  "pish_f411_sfr.h"
+#include "stdint.h"
 
 #ifndef INC_PISH_UART_DRV_H_
 #define INC_PISH_UART_DRV_H_
 
-void PISH_UART_Int();
+typedef struct
+{
+	uint32_t speed;
+	uint32_t parity : 1U;
+	uint32_t stopbits : 1U;
+	uint32_t databits : 2U;
+} UARTParam_t;
 
+void PISH_UART_Int(USARTx_t* uart, UARTParam_t* params);
+uint8_t PISH_UART_ReadSync(USARTx_t* uart);
+
+void PISH_UART_WriteSync(USARTx_t* uart, uint8_t data);
+
+void PISH_UART_WriteStr(USARTx_t* uart, uint8_t* data);
+
+void PISH_UART_SetCallback(void(*callback)(uint8_t* data));
 #endif /* INC_PISH_UART_DRV_H_ */
